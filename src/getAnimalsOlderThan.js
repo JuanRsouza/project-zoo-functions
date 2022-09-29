6cbc32eee97ff8f5dbeda7aed81e63dc10363dd6
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-09-29 19:03:13 -0300

    Fazendo o requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -9,14 +9,3 @@ function getAnimalsOlderThan(animal, age) {
 console.log(getAnimalsOlderThan('lions', 7));
 
 module.exports = getAnimalsOlderThan;
-
-// Implemente a função getAnimalsOlderThan que deve receber uma espécie e uma idade como parâmetro, e então retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos.
-
-// Verifique se todos os animais da espécie passada como parâmetro possuem a idade mínima:
-
-// Os animais devem ter essa idade ou serem mais velhos.
-// Retorne um valor booleano.
-
-// O que será testado:
-
-// A função, ao receber uma espécie e uma idade como parâmetros, deve testar se todos os animais desta espécie possuem a idade mínima especificada.
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -11,11 +11,3 @@ function getSpeciesByIds(...ids) {
 getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '89be95b3-47e4-4c5b-b687-1fabf2afa274');
 
 module.exports = getSpeciesByIds;
-
-// REQUISITO 1 :
-
-//  implementar uma função que busque as espécies dos animais pelo ID e retorne um array com os animais dessa espécie.
-//  oBS: a função deve receber vários parâmetros.
-// deve retornar um array vazio se a função não receber um ID. (um parâmetro)
-// se a função receber apenas um id, retorne a espécie do animal referente a este id.
-// se a função receber vários ids, retorne todas as espécies referente a esses ids.
