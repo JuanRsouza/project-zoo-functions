ebee85b2d07f3837a0411c2267c911d410f60c21
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-09-29 18:41:58 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = data.species.find((element) => element.name === animal);
+  const getAge = getAnimal.residents.every((elemento) => elemento.age >= age);
+  return getAge;
 }
 
+console.log(getAnimalsOlderThan('lions', 7));
+
 module.exports = getAnimalsOlderThan;
+
+// Implemente a função getAnimalsOlderThan que deve receber uma espécie e uma idade como parâmetro, e então retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos.
+
+// Verifique se todos os animais da espécie passada como parâmetro possuem a idade mínima:
+
+// Os animais devem ter essa idade ou serem mais velhos.
+// Retorne um valor booleano.
+
+// O que será testado:
+
+// A função, ao receber uma espécie e uma idade como parâmetros, deve testar se todos os animais desta espécie possuem a idade mínima especificada.
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const filteredSpeciesID = data.species.filter((element) => {
+    const inclui = ids.includes(element.id);
+    return inclui;
+  });
+  return filteredSpeciesID;
 }
 
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '89be95b3-47e4-4c5b-b687-1fabf2afa274');
+
 module.exports = getSpeciesByIds;
+
+// REQUISITO 1 :
+
+//  implementar uma função que busque as espécies dos animais pelo ID e retorne um array com os animais dessa espécie.
+//  oBS: a função deve receber vários parâmetros.
+// deve retornar um array vazio se a função não receber um ID. (um parâmetro)
+// se a função receber apenas um id, retorne a espécie do animal referente a este id.
+// se a função receber vários ids, retorne todas as espécies referente a esses ids.
