618c2b834d9d42a03984ad36f9bbb5c6389c73f4
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-09-30 17:23:05 -0300

    Finalizei a 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const noParam = () => {
+  const obj = {};
+  data.species.forEach((el) => {
+    obj[el.name] = el.residents.length;
+  });
+  return obj;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) return noParam();
+  const especie = data.species.find((esp) => animal.specie === esp.name);
+  const lengthResidents = especie.residents.length;
+  if (animal.sex) {
+    const filtered = especie.residents.filter((el) => el.sex === animal.sex);
+    const numSex = filtered.length;
+    return numSex;
+  }
+  return lengthResidents;
 }
+
+// eslint-disable-next-line sonarjs/no-use-of-empty-return-value
+countAnimals({ specie: 'elephants', sex: 'male' });
 
 module.exports = countAnimals;
