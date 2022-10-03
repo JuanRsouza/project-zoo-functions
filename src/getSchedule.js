20a38b43e957ebfa97ce8ff19d5cd3bd16f659c2
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-10-03 20:09:50 -0300

    Parábens, o requisito 9 quase explodiu minha mente

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,22 +1,46 @@
 const data = require('../data/zoo_data');
 
-function daysAvailable(scheduleTarget) {
+const diasDaSemana = Object.keys(data.hours);
+
+const animals = data.species.map(({name}) => name);
+
+function animalsAvailable(scheduleTarget) {
   const diasDisponiveis = data.species.find((e) => e.name === scheduleTarget);
-  const arrayDisponibilidade = diasDisponiveis.availability.filter((d) => d);
-  return arrayDisponibilidade;
+  return diasDisponiveis.availability;
 }
 
-function noParam(scheduleTarget) {
-  const animalsAndDays = data.species.filter((el) => el.availability === (data.hours));
-  console.log(animalsAndDays);
+function exHibition(dia) {
+  const filterSpecies = data.species.filter((animal) => animal.availability
+    .includes(dia))
+    .map(({ name }) => name);
+
+  return filterSpecies;
+}
+
+function noParam() {
+  const obj = {};
+  diasDaSemana.forEach((dia) => {
+    obj[dia] = {
+      officeHour: `Open from ${data.hours[dia].open}am until ${data.hours[dia].close}pm`,
+      exhibition: exHibition(dia),
+    };
+    if (dia === 'Monday') {
+      obj[dia] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+  return obj;
 }
 
 function getSchedule(scheduleTarget) {
-  if (!scheduleTarget) return noParam();
-  return daysAvailable(scheduleTarget);
+  if (diasDaSemana.includes(scheduleTarget)) return { [scheduleTarget]: noParam()[scheduleTarget] };
+  if (animals.includes(scheduleTarget)) return animalsAvailable(scheduleTarget);
+  return noParam();
 }
 
-console.log(getSchedule('lions'));
-console.log(noParam());
+console.log(getSchedule('Tuesday'));
+// console.log(noParam());
 
 module.exports = getSchedule;
