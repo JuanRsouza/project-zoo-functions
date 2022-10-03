62cc85aaa4dbb552eb7bc99edc83e6be35022a81
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-10-03 16:25:54 -0300

    Fazendo o requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function daysAvailable(scheduleTarget) {
+  const diasDisponiveis = data.species.find((e) => e.name === scheduleTarget);
+  const arrayDisponibilidade = diasDisponiveis.availability.filter((d) => d);
+  return arrayDisponibilidade;
 }
+
+function noParam(scheduleTarget) {
+  const animalsAndDays = data.species.filter((el) => el.availability === (data.hours));
+  console.log(animalsAndDays);
+}
+
+function getSchedule(scheduleTarget) {
+  if (!scheduleTarget) return noParam();
+  return daysAvailable(scheduleTarget);
+}
+
+console.log(getSchedule('lions'));
+console.log(noParam());
 
 module.exports = getSchedule;
